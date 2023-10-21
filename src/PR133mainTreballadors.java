8a264b5f9be8e9d45c66ccc0ab92aa5f3dcbae22
Arguelless <marguelles@protonmail.com>
2023-10-21 16:50:34 +0200

    asfd

--- src/PR133mainTreballadors.java
+++ src/PR133mainTreballadors.java
@@ -1,3 +1,42 @@
+import java.util.List;
+import java.util.Scanner;
+
+
 public class PR133mainTreballadors {
-    
+    static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        boolean running = true;
+        String filePath = "./src/PR133treballadors.csv";
+
+        while (running) {
+            
+            String mete_id = "Introduce la ID del trabajador: ";
+            System.out.println(mete_id);
+
+
+            try {
+                int opcio = Integer.valueOf(llegirLinia("ID: "));
+
+                
+                if (opcio == 0) {
+                    System.out.println("No existe el trabajador");
+                } else {
+                    System.out.println();
+                }
+            
+            } catch (NumberFormatException e) {
+                System.out.println("Introduce un número");
+            }
+             catch (Exception e) {
+                System.out.println(e);
+            }
+            List<String> csvLines = UtilsCSV.read(filePath);
+            
+            System.out.println(UtilsCSV.getColumnData(csvLines, "1"));
+        }
+    }
+    static public String llegirLinia (String text) {
+        System.out.print(text);
+        return in.nextLine();
+    }
 }
