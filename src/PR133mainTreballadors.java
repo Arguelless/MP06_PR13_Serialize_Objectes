dd0269d6a22bb3f9e8aea6cbd3fe502acfdd6022
Arguelless <marguelles@protonmail.com>
2023-10-21 18:10:38 +0200

    asdf

--- src/PR133mainTreballadors.java
+++ src/PR133mainTreballadors.java
@@ -1,41 +1,86 @@
 import java.util.List;
 import java.util.Scanner;
 
-
 public class PR133mainTreballadors {
     static Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) {
         boolean running = true;
         String filePath = "./src/PR133treballadors.csv";
 
         while (running) {
-            
-            String mete_id = "Introduce la ID del trabajador: ";
-            System.out.println(mete_id);
 
+            System.out.println("Introduce la ID del trabajador: ");
+            List<String> csvLines = UtilsCSV.read(filePath);
+            String[] ids = UtilsCSV.getColumnData(csvLines, "id");
 
             try {
-                int opcio = Integer.valueOf(llegirLinia("ID: "));
-
-                
-                if (opcio == 0) {
-                    System.out.println("No existe el trabajador");
-                } else {
-                    System.out.println();
+                String opcio = llegirLinia("ID (0 para salir): ");
+                if (opcio.equals("0")) {
+                    running = false;
+                    break;
                 }
-            
+                Boolean encontrado = false;
+
+                for (String id : ids) {
+                    if (id.equals(opcio)) {
+                        encontrado = true;
+                        break;
+                    }
+                }
+                if (encontrado) {
+
+                    String[] keys = UtilsCSV.getKeys(csvLines);
+                    System.out.println("Datos del trabajador: ");
+                    for (String key : keys) {
+                        System.out.println(" - " + key + ": " + UtilsCSV.getColumnData(csvLines, key)[UtilsCSV.getLineNumber(csvLines, "id", opcio)]);
+                    }
+
+                    boolean runningModif = true;
+                    while (runningModif) {
+                        System.out.println("Introduce el parametro a modificar: ");
+                        for (String key : keys) {
+                            if (keys[keys.length - 1] == key) {
+                                System.out.println(key);
+                                break;
+                            }
+                            System.out.print(key + ", ");
+
+                        }
+                        String parametro = llegirLinia("Parametro (0 para salir): ");
+                        if (parametro.equals("0")) {
+                            runningModif = false;
+                            break;
+                        }
+                        int columnPosition = UtilsCSV.csvGetColumnPosition(csvLines, parametro);
+
+                        if (columnPosition != -1) {
+                            System.out.println("Introduce el nuevo valor: ");
+                            String valor = llegirLinia("Valor: ");
+
+                            UtilsCSV.update(csvLines, UtilsCSV.getLineNumber(csvLines, "id", opcio), parametro, valor);
+
+                            UtilsCSV.write(filePath, csvLines);
+                            System.out.println("Valor modificado correctamente");
+                            runningModif = false;
+                        } else {
+                            System.out.println("Parametro no encontrado");
+                        }
+                    }
+                } else {
+                    System.out.println("ID no encontrada");
+                }
+
             } catch (NumberFormatException e) {
                 System.out.println("Introduce un número");
-            }
-             catch (Exception e) {
+            } catch (Exception e) {
                 System.out.println(e);
             }
-            List<String> csvLines = UtilsCSV.read(filePath);
-            
-            System.out.println(UtilsCSV.getColumnData(csvLines, "1"));
+
         }
     }
-    static public String llegirLinia (String text) {
+
+    static public String llegirLinia(String text) {
         System.out.print(text);
         return in.nextLine();
     }
